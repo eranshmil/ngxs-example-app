55ca5f8be9bca584f26e0c5f31a266d0404a7172
eranshmil <eranshmil@users.noreply.github.com>
2018-11-11 15:10:47 +0200

    chore(jest): cleaner packge.json

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'src/tsconfig.spec.json',
+    },
+    __TRANSFORM_HTML__: true,
+  },
+  transform: {
+    '^.+\\.(ts|js|html)$':
+      '<rootDir>/node_modules/jest-preset-angular/preprocessor.js',
+  },
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  modulePathIgnorePatterns: ['dist'],
+};
