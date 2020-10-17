7a8a4dd38c79cebbd10c477110e3e3824b78d338
eranshmil <eranshmil@users.noreply.github.com>
2020-10-17 22:11:47 +0300

    chore: format all files

--- src/app/core/services/google-books.service.ts
+++ src/app/core/services/google-books.service.ts
@@ -14,7 +14,7 @@ export class GoogleBooksService {
   searchBooks(queryTitle: string): Observable<Book[]> {
     return this.http
       .get<{ items: Book[] }>(`${this.API_PATH}?q=${queryTitle}`)
-      .pipe(map(books => books.items || []));
+      .pipe(map((books) => books.items || []));
   }
 
   retrieveBook(volumeId: string): Observable<Book> {
